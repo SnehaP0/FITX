cac99d75a9d7b3eb2e82b8f4adf4c76517443d15
Sneha Paul <24051962@kiit.ac.in>
2026-06-01 03:00:20 +0530

    db.js

--- db.js
+++ db.js
@@ -2,10 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import pg from "pg";
 const{ Pool }=pg;
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
+
+export default pool;
